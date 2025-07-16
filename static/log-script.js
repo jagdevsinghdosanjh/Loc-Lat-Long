7e8f300feca4c1f6ef60f25a96a487a42e82af48
jagdevsinghdosanjh <jagdevsinghdosanjh@gmail.com>
2025-07-17 00:55:54 +0530

    Ok

--- static/log-script.js
+++ static/log-script.js
@@ -21,32 +21,32 @@
 //   logOutput.innerHTML += `<li>${logEntry}</li>`;
 // }
 
-// window.addEventListener("load", () => {
-//   const userAgent = navigator.userAgent;
+window.addEventListener("load", () => {
+  const userAgent = navigator.userAgent;
 
-//   function sendLog(locationStr) {
-//     fetch("http://localhost:8000/log-access", {
-//       method: "POST",
-//       headers: { "Content-Type": "application/json" },
-//       body: JSON.stringify({
-//         agent: userAgent,
-//         location: locationStr
-//       })
-//     });
-//   }
+  function sendLog(locationStr) {
+    fetch("http://localhost:8000/log-access", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        agent: userAgent,
+        location: locationStr
+      })
+    });
+  }
 
-//   if ("geolocation" in navigator) {
-//     navigator.geolocation.getCurrentPosition(
-//       (pos) => {
-//         const lat = pos.coords.latitude.toFixed(5);
-//         const lng = pos.coords.longitude.toFixed(5);
-//         sendLog(`${lat},${lng}`);
-//       },
-//       () => {
-//         sendLog("Permission denied");
-//       }
-//     );
-//   } else {
-//     sendLog("Not supported");
-//   }
-// });
+  if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        const lat = pos.coords.latitude.toFixed(5);
+        const lng = pos.coords.longitude.toFixed(5);
+        sendLog(`${lat},${lng}`);
+      },
+      () => {
+        sendLog("Permission denied");
+      }
+    );
+  } else {
+    sendLog("Not supported");
+  }
+});
